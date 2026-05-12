dbc411a949e92ecac83caf427737b0bfb4aa26d3
utagawastudio1128-gif <utagawastudio1128@gmail.com>
2026-05-12 13:01:42 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // 【重要】FirebaseのRealtime Database URLをここに貼り付けてください
 export const firebaseConfig = {
-    databaseURL: "https://nanndemoiiyo-8c327-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: "https://nanndemoiiyo-8c327-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
